33b7b9a1089141d586e1f7af9e054c3d4e5fc4b2
Aman Thukral <thukral618@gmail.com>
2020-07-30 03:58:15 +0530

    setting up api to fetch data

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   MenuItem,
   FormControl,
   Select
-} from '@material-ui/core';
-
+} from "@material-ui/core";
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => (
+            {
+              name: country.country,
+              value: country.countryInfo.iso2
+            }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, [])
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+
+    console.log("helloo", countryCode);
+    setCountry(countryCode);
+  }
+
   return (
     <div className="app">
       <div className="app__header">
         <h1>COVID 19 Tracker</h1>
         <FormControl className="app__dropdown">
-          <Select
-            variant="outlined"
-            value="abc"
-          >
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
             <MenuItem value="worldwide">Worldwide</MenuItem>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map(country => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
           </Select>
         </FormControl>
       </div>
