731300c8944fe86e3b3638dc737b80bf872a6506
Aman Thukral <thukral618@gmail.com>
2020-07-31 05:23:36 +0530

    adding components

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -2,8 +2,12 @@ import React, { useState, useEffect } from 'react';
 import {
   MenuItem,
   FormControl,
-  Select
+  Select,
+  Card,
+  CardContent
 } from "@material-ui/core";
+import InfoBox from './InfoBox';
+import Map from "./Map";
 import './App.css';
 
 function App() {
@@ -28,37 +32,51 @@ function App() {
 
   const onCountryChange = async (event) => {
     const countryCode = event.target.value;
-
-    console.log("helloo", countryCode);
     setCountry(countryCode);
   }
 
   return (
     <div className="app">
-      <div className="app__header">
-        <h1>COVID 19 Tracker</h1>
-        <FormControl className="app__dropdown">
-          <Select variant="outlined" onChange={onCountryChange} value={country}>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            {countries.map(country => (
-              <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COVID 19 Tracker</h1>
+          <FormControl className="app__dropdown">
+            <Select variant="outlined" onChange={onCountryChange} value={country}>
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map(country => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app__stats">
+          <InfoBox title="Coronavirus Cases" cases={123} total={2000} />
+
+          <InfoBox title="Recovered" cases={1234} total={3000} />
+
+          <InfoBox title="Deaths" cases={12345} total={4000} />
+
+        </div>
+
+
+
+
+
+
+
+
+        {/* Map*/}
+        <Map />
       </div>
-
-
-      {/* Header*/}
-      {/* Title+select input dropdown field*/}
-
-      {/* InfoBox*/}
-      {/* infobox*/}
-      {/* infobox*/}
-
-      {/* table*/}
-      {/* graph*/}
-
-      {/* Map*/}
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live cases by country</h3>
+          <h3>Worldwide new cases</h3>
+        </CardContent>
+        {/* table*/}
+        {/* graph*/}
+      </Card>
     </div>
   );
 }
